63163f1361e6f12fd6381f1757d0cad6ef30b9c5
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2024-10-20 11:51:48 +0100

    Main | Setup init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<>
+			<div className='bg-red-300'>
+				Load other components here .. no special needs for routing per the
+				design !!!
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+        },
+      },
+      fontFamily: {
+        schibsted: ['Schibsted Grotesk', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
