2e9f8a2b503881ab38eb23b44adcc0bf449bbd7b
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2024-10-23 18:37:18 +0100

    Main | Fix css config and formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,27 +11,21 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
-        },
-        screens: {
-          sm: '640px',
-          md: '768px',
-          lg: '1024px',
-          xl: '1280px',
-          '2xl': '1536px',
-        },
-      },
       fontFamily: {
         schibsted: ['Schibsted Grotesk', 'sans-serif'],
+        lobster: ['Lobster', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-light':
+          'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;',
+      },
+      colors: {
+        'primary-dark': '#484554',
+        'alpha-violet': '#8576FF',
+        'base-dark': '#38343F',
       },
     },
   },
   plugins: [],
-}
+  important: true,
+};
